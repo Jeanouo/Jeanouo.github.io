d6e955e33fb41fb055f96819c7a207c73dba3aef
jeanouo <s860998@yahoo.com.tw>
2019-11-22 18:08:19 +0800

    menu 做到一半..

--- js/welcome.js
+++ js/welcome.js
@@ -1,15 +1,18 @@
+// for 歡迎頁面-滑入按鈕時的狀態
 $('.welcomeBTN').mouseover(function(){
     $('.welcome-absolute').css('align-items','center');
     $('.welcome-absolute p').hide('slow');
     $('.welcome-absolute h4').text('Enter').css({'font-size':'200%','padding-bottom':'5px','border-bottom':'2px solid #fff'});
 });
 
+// for 歡迎頁面-滑出按鈕時的狀態
 $('.welcomeBTN').mouseout(function(){
     $('.welcome-absolute').css('align-items','flex-end');
     $('.welcome-absolute p').show('slow');
     $('.welcome-absolute h4').text('JCDS').css({'font-size':'24px','padding-bottom':'none','border-bottom':'none'});
 });
 
+// for 歡迎頁面-點擊按鈕,進入畫面
 function enterJCDS() {
     $('.welcomeBg-pink').addClass('pink-cover');
     setTimeout(function(){
@@ -20,3 +23,15 @@ function enterJCDS() {
         $('.jc-content').css('max-height','unset');
     }, 1000);
 }
+
+// for 歡迎頁面-過5s後跑時間軸,進入畫面
+// setTimeout(function(){
+//     $('.timeline-running').css('width','100%');
+//     setTimeout(function(){
+//         $('.welcomeBg').css('opacity','0');
+//     }, 2000);
+//     setTimeout(function(){
+//         $('.welcomeBg').css('display','none');
+//         $('.jc-content').css('max-height','unset');
+//     }, 3000);
+// }, 5000);
