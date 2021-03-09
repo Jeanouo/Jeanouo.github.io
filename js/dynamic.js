0251b32144aa17ba02bf3f1d05831cef14aca35b
jean <50897212+Jeanouo@users.noreply.github.com>
2021-03-09 18:14:43 +0800

    learn SEO

--- js/dynamic.js
+++ js/dynamic.js
@@ -112,11 +112,13 @@ $('.sec-title').click(function(){
     $(this).parent().siblings().children('.learn-content-wrapper').slideUp();
     $(this).next('.learn-content-wrapper').slideToggle();
 });
-$(function(){
-    $('.liearn-webNside-1').click(function(){
-        $('.learn-content-wrapper').slideUp();
-        $('#liearn-webNside-1 ~ .learn-content-wrapper').slideDown();
-    });
+$('.liearn-webNside-1').click(function(){
+    $('.learn-content-wrapper').slideUp();
+    $('#liearn-webNside-1 ~ .learn-content-wrapper').slideDown();
+});
+$('.liearn-SEOrelatedTools-1').click(function(){
+    $('.learn-content-wrapper').slideUp();
+    $('#liearn-SEOrelatedTools-1 ~ .learn-content-wrapper').slideDown();
 });
 
 
